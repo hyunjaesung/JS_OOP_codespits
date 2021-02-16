37d91eb8d033600e12f6fe794f306910420d4a95
Steve Sung <steveysung91@gmail.com>
2021-02-16 15:15:39 +0900

    viewModel

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
-import "./styles.css";
+const ViewModel = class {
+  static #private = Symbol();
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+  static get(data) {
+    // static으로 ViewModel.get() 으로만 뷰모델 생성 가능
+    return new ViewModel(this.#private, data);
+  }
+
+  styles = {};
+  attribute = {};
+  properties = {};
+  events = {};
+
+  constructor(checker, data) {
+    if (checker != ViewModel.#private) throw "use ViewModel.get() !!";
+    // 외부에서는 생성 불가능하도록
+    Object.entries(data).forEach(([key, value]) => {
+      switch (key) {
+        case "styles":
+          this.styles = value;
+          break;
+        case "attribute":
+          this.attribute = value;
+          break;
+        case "properties":
+          this.properties = value;
+          break;
+        case "events":
+          this.events = value;
+          break;
+        default:
+          this[key] = value;
+      }
+    });
+    Object.seal(this); // 더이상 키 추가 불가
+  }
+};
